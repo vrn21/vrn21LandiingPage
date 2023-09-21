de410ed392bb1c6e1593ee60663dc45c5c864b30
K V Varun Krishnan <varunkkv.mec@gmail.com>
2023-09-22 01:39:09 +0530

    completed site, allignment issues persist

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,8 +3,8 @@ import React from "react"
 const NavBar = () =>{
     return(
         <nav>
-            <a href="https://linktr.ee/vrn21XD" className = "linktree">linktree</a>
-            <a href="https://www.figma.com/file/lLZ2Vxs5FLIAhwlUDUuC0V/PortFolio?node-id=0:1&mode=dev" className="design-portfolio"> design-portfolio</a>
+            <a href="https://linktr.ee/vrn21XD" className = "linktree" text-decoration = "none" >linktree</a>
+            <a href="https://www.figma.com/file/lLZ2Vxs5FLIAhwlUDUuC0V/PortFolio?node-id=0:1&mode=dev" className="design-portfolio" text-decoration = "none"  > design-portfolio</a>
         </nav>
     )
 }
