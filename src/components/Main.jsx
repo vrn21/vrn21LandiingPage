91d9a6222a270cab74d90587b19fc751490af36c
K V Varun Krishnan <varunkkv.mec@gmail.com>
2023-11-06 20:58:42 +0530

    temporarily completed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,11 +2,11 @@ import React from "react"
 
 const Main = () => { 
     return(
-        <div>
-            <p className = "l1">gm, im</p>
-            <h1>vrn21</h1>
-            <p className= "l2">aka K V Varun Krishnan</p>
-        </div>
+            <div className ="Main">
+                <p className = "l1">gm, im</p>
+                <h1 className ="H1">vrn21</h1>
+                <p className= "l2">aka K V Varun Krishnan</p>
+            </div>
     )
 
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,8 +5,10 @@ const NavBar = () =>{
         <nav>
             <a href="https://linktr.ee/vrn21XD" className = "linktree" text-decoration = "none" >linktree</a>
             <a href="https://www.figma.com/file/lLZ2Vxs5FLIAhwlUDUuC0V/PortFolio?node-id=0:1&mode=dev" className="design-portfolio" text-decoration = "none"  > design-portfolio</a>
+            <a href="https://linktr.ee/vrn21XD" className = "linktree" text-decoration = "none" >CoinFLow</a>
         </nav>
     )
 }
 
 export default NavBar
+
