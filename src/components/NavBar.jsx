c41a5b69d1f84fea93cdab3ec3574d18acdb7499
K V Varun Krishnan <varunkkv.mec@gmail.com>
2023-11-08 00:08:55 +0530

    Updated navbar link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () =>{
         <nav>
             <a href="https://linktr.ee/vrn21XD" className = "linktree" text-decoration = "none" >linktree</a>
             <a href="https://www.figma.com/file/lLZ2Vxs5FLIAhwlUDUuC0V/PortFolio?node-id=0:1&mode=dev" className="design-portfolio" text-decoration = "none"  > design-portfolio</a>
-            <a href="https://linktr.ee/vrn21XD" className = "linktree" text-decoration = "none" >CoinFLow</a>
+            <a href="https://coinflow-sandy.vercel.app/" className = "linktree" text-decoration = "none" >CoinFLow</a>
         </nav>
     )
 }
